950aee58c14f49c79ff1ececc04bd2e465d8ff3d
Wael-Hx <wael.lhb@gmail.com>
2021-02-10 03:03:45 +0100

    relative unix time fn

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -8,12 +8,27 @@ export const relativeTime = (unixTime) => {
   let currentTime = Math.round(new Date());
   currentTime = Math.round(currentTime / 1000);
   let relativeTime = currentTime - unixTime;
-  if (relativeTime < 3600) {
-    return `${Math.round(relativeTime / 60)} min ago`;
-  } else if (relativeTime < 86400) {
-    return `${Math.round(relativeTime / 60 / 60)} hours ago `;
-  } else if (relativeTime > 86400) {
-    return `${Math.round(relativeTime / 60 / 60 / 24)} days ago `;
+  let min = 60,
+    hour = 3600,
+    day = 86400,
+    month = 2592000,
+    rTime = 0;
+
+  if (relativeTime < min) {
+    rTime = Math.round(relativeTime / 1);
+    return `${rTime} second${rTime > 1 ? "s" : ""} ago`;
+  } else if (relativeTime < hour) {
+    rTime = Math.round(relativeTime / min);
+    return `${rTime} min${rTime > 1 ? "s" : ""} ago`;
+  } else if (relativeTime < day) {
+    rTime = Math.round(relativeTime / hour);
+    return `${rTime} hour${rTime > 1 ? "s" : ""} ago `;
+  } else if (relativeTime > day && relativeTime < month) {
+    rTime = Math.round(relativeTime / day);
+    return `${rTime} day${rTime > 1 ? "s" : ""} ago `;
+  } else if (relativeTime > month) {
+    rTime = Math.round(relativeTime / month);
+    return `${rTime} month${rTime > 1 ? "s" : ""} ago `;
   } else {
     return "unknown date format";
   }
